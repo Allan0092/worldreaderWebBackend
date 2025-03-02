1c055b09744c8f6d4fdc98682064719f44ef1c08
allan <allan.gtm92@gmail.com>
2025-03-02 06:47:46 +0545

    added static file_storage

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express=require("express")
-const connectDb=require("./config/db")
-const UserRouter=require("./routes/UserRoute")
-const BookRouter=require("./routes/BookRoute")
-const AuthRouter=require("./routes/AuthRoute")
+const express = require("express");
+const connectDb = require("./config/db");
+const UserRouter = require("./routes/UserRoute");
+const BookRouter = require("./routes/BookRoute");
+const AuthRouter = require("./routes/AuthRoute");
 const cors = require("cors");
+const path = require("path");
 
-const app=express();
+const app = express();
 connectDb();
 
 app.use(cors());
 app.use(express.json());
+app.use("/file_storage", express.static(path.join(__dirname, "file_storage"))); // Serve static files
 
 app.use("/api/user", UserRouter);
 app.use("/api/book", BookRouter);
 app.use("/auth", AuthRouter);
 
-PORT = 5000
-app.listen(PORT, () =>{
-    console.log(`Server started at http://localhost:${PORT}`);
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
 });
